f5e4b34415f2169f01aef30b36a5b70edfa7e27e
Aloecheverria7 <33984928+Aloecheverria7@users.noreply.github.com>
2023-02-24 13:09:13 -0600

    zip
    bio gallery

--- php/secction/seccion-video.php
+++ php/secction/seccion-video.php
@@ -1,15 +1,15 @@
 <section id="mt_get_started">
-        <div class="container">
-            <div class="watch_inner">
-                <img src="img/show.png" alt="">
-                <div class="watch_content">
-                    <h3 class="white"><?php echo $Video_Text[0]  ?></h3>
-                    <h2 class="white"><?php echo $Video_Text[1]  ?></h2>
-                    <p><?php echo $Video_Text[2]  ?></p>
-                    <a href="gallery.php" class="btn mt_btn_yellow mar-top-30">View Work Process</a>
-                </div>
-                <div class="pulses">
-                </div>
+    <div class="container">
+        <div class="watch_inner">
+            <img src="img/show.png" alt="">
+            <div class="watch_content">
+                <h3 class="white"><?php echo $Video_Text[0]  ?></h3>
+                <h2 class="white"><?php echo $Video_Text[1]  ?></h2>
+                <p><?php echo $Video_Text[2]  ?></p>
+                <a href="gallery.php" class="btn mt_btn_yellow mar-top-30">View Work Process</a>
+            </div>
+            <div class="pulses">
             </div>
         </div>
-    </section>
+    </div>
+</section>
